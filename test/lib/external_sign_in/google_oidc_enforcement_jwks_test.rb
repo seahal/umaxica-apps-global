b76e80779bb7ac088de734cc1f9ed9669466fad0
seahal <m.shiihara@gmail.com>
2026-08-27 04:55:09 +0000

    [FIX] Inline Google JWKS HTTP stubs in focused tests
    The Minitest contract forbids custom test helpers. Each JWKS response
    scenario now stubs Net::HTTP at the point of use so failures stay
    independent.

--- test/lib/external_sign_in/google_oidc_enforcement_jwks_test.rb
+++ test/lib/external_sign_in/google_oidc_enforcement_jwks_test.rb
@@ -4,49 +4,92 @@
 require "test_helper"
 
 class GoogleOidcEnforcementJwksTest < ActiveSupport::TestCase
-  test "loads google jwks through cache and rejects invalid payloads" do
+  test "fetch_google_jwks returns parsed keys from a successful JWKS response" do
     subject = Object.new
     subject.extend(ExternalAuthenticationInfrastructureOmniauthGoogleOidcEnforcement)
     Rails.cache.delete(ExternalAuthenticationInfrastructureOmniauthGoogleOidcEnforcement::JWKS_CACHE_KEY)
 
-    def stub_google_http(response)
-      http = Object.new
-      http.define_singleton_method(:request) { |_request| response }
-      Net::HTTP.stub(:start, ->(*_args, **_kwargs, &_block) { _block.call(http) }) { yield }
-    end
-
     success = Net::HTTPSuccess.new("1.1", "200", "OK")
     success.define_singleton_method(:body) { '{"keys":[{"kid":"k1"}]}' }
-    stub_google_http(success) do
+    http = Object.new
+    http.define_singleton_method(:request) { |_request| success }
+
+    Net::HTTP.stub(:start, ->(*_args, **_kwargs, &block) { block.call(http) }) do
       jwks = subject.send(:fetch_google_jwks)
 
       assert_equal [{ "kid" => "k1" }], jwks.fetch("keys")
     end
+  end
 
-    loader = subject.send(:google_jwks_loader)
-    stub_google_http(success) do
-      assert_equal [{ "kid" => "k1" }], loader.call.fetch("keys")
-      loader.call(kid_not_found: true)
+  test "google_jwks_loader refetches when the requested key is missing" do
+    subject = Object.new
+    subject.extend(ExternalAuthenticationInfrastructureOmniauthGoogleOidcEnforcement)
+    Rails.cache.delete(ExternalAuthenticationInfrastructureOmniauthGoogleOidcEnforcement::JWKS_CACHE_KEY)
+
+    success = Net::HTTPSuccess.new("1.1", "200", "OK")
+    success.define_singleton_method(:body) { '{"keys":[{"kid":"k1"}]}' }
+    requests = 0
+    http = Object.new
+    http.define_singleton_method(:request) do |_request|
+      requests += 1
+      success
     end
 
+    Net::HTTP.stub(:start, ->(*_args, **_kwargs, &block) { block.call(http) }) do
+      loader = subject.send(:google_jwks_loader)
+
+      assert_equal [{ "kid" => "k1" }], loader.call.fetch("keys")
+      assert_equal 1, requests
+
+      assert_equal [{ "kid" => "k1" }], loader.call(kid_not_found: true).fetch("keys")
+      assert_equal 2, requests
+    end
+  end
+
+  test "fetch_google_jwks raises when Google returns a non-success response" do
+    subject = Object.new
+    subject.extend(ExternalAuthenticationInfrastructureOmniauthGoogleOidcEnforcement)
+    Rails.cache.delete(ExternalAuthenticationInfrastructureOmniauthGoogleOidcEnforcement::JWKS_CACHE_KEY)
+
     failure = Net::HTTPResponse.new("1.1", "500", "ERR")
-    stub_google_http(failure) do
+    http = Object.new
+    http.define_singleton_method(:request) { |_request| failure }
+
+    Net::HTTP.stub(:start, ->(*_args, **_kwargs, &block) { block.call(http) }) do
       error = assert_raises(JWT::DecodeError) { subject.send(:fetch_google_jwks) }
 
       assert_equal "Google JWKS fetch failed", error.message
     end
+  end
+
+  test "fetch_google_jwks raises when the JWKS body is not JSON" do
+    subject = Object.new
+    subject.extend(ExternalAuthenticationInfrastructureOmniauthGoogleOidcEnforcement)
+    Rails.cache.delete(ExternalAuthenticationInfrastructureOmniauthGoogleOidcEnforcement::JWKS_CACHE_KEY)
 
     bad_json = Net::HTTPSuccess.new("1.1", "200", "OK")
     bad_json.define_singleton_method(:body) { "not-json" }
-    stub_google_http(bad_json) do
+    http = Object.new
+    http.define_singleton_method(:request) { |_request| bad_json }
+
+    Net::HTTP.stub(:start, ->(*_args, **_kwargs, &block) { block.call(http) }) do
       error = assert_raises(JWT::DecodeError) { subject.send(:fetch_google_jwks) }
 
       assert_equal "JSON::ParserError", error.message
     end
+  end
+
+  test "fetch_google_jwks raises when keys is not an array" do
+    subject = Object.new
+    subject.extend(ExternalAuthenticationInfrastructureOmniauthGoogleOidcEnforcement)
+    Rails.cache.delete(ExternalAuthenticationInfrastructureOmniauthGoogleOidcEnforcement::JWKS_CACHE_KEY)
 
     invalid_shape = Net::HTTPSuccess.new("1.1", "200", "OK")
     invalid_shape.define_singleton_method(:body) { '{"keys":"nope"}' }
-    stub_google_http(invalid_shape) do
+    http = Object.new
+    http.define_singleton_method(:request) { |_request| invalid_shape }
+
+    Net::HTTP.stub(:start, ->(*_args, **_kwargs, &block) { block.call(http) }) do
       error = assert_raises(JWT::DecodeError) { subject.send(:fetch_google_jwks) }
 
       assert_equal "Google JWKS has an invalid shape", error.message
